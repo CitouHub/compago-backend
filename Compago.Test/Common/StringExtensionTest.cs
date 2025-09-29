8e329464e800ed1615f8dd50f4c9012356ae205a
CitouHub <218735@skane.se>
2025-09-29 22:50:37 +0200

    ONE extra inline data

--- Compago.Test/Common/StringExtensionTest.cs
+++ Compago.Test/Common/StringExtensionTest.cs
@@ -12,6 +12,7 @@ namespace Compago.Test.Common
         [InlineData("#7890AB", true)]
         [InlineData("#0000000", false)]
         [InlineData("#00000", false)]
+        [InlineData("##00000", false)]
         [InlineData("#G00000", false)]
         [InlineData("#0G0000", false)]
         [InlineData("#00G000", false)]
