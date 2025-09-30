396a7d01615a83bb9dcdef2f9d05ad5d9b2753fd
CitouHub <218735@skane.se>
2025-09-30 18:10:50 +0200

    NOW!

--- Compago.Service/CurrencyService.cs
+++ Compago.Service/CurrencyService.cs
@@ -31,39 +31,39 @@ namespace Compago.Service
                 //// #############################
                 //// ## HTTP Simulated API Call ##
                 //// #############################
-                //var cancellationTokenSource = new CancellationTokenSource();
-                //var cancellationToken = cancellationTokenSource.Token;
-                //var task = new Task<double>(_ =>
-                //{
-                //    try
-                //    {
-                //        var longBytes = BitConverter.GetBytes((double)0).ToList();
+                var cancellationTokenSource = new CancellationTokenSource();
+                var cancellationToken = cancellationTokenSource.Token;
+                var task = new Task<double>(_ =>
+                {
+                    try
+                    {
+                        var longBytes = BitConverter.GetBytes((double)0).ToList();
 
-                //        var baseFactorKey = $"{fromCurrency}{toCurrency}";
-                //        var baseFactorKeyBytes = Encoding.ASCII.GetBytes(baseFactorKey).ToList().Concat(longBytes);
-                //        var baseFactorKeyLong = BitConverter.ToInt64([.. baseFactorKeyBytes], 0);
-                //        var baseFactorBase = baseFactorKeyLong % 10;
-                //        var baseFactorInverse = baseFactorKeyLong % 2 == 0;
+                        var baseFactorKey = $"{fromCurrency}{toCurrency}";
+                        var baseFactorKeyBytes = Encoding.ASCII.GetBytes(baseFactorKey).ToList().Concat(longBytes);
+                        var baseFactorKeyLong = BitConverter.ToInt64([.. baseFactorKeyBytes], 0);
+                        var baseFactorBase = baseFactorKeyLong % 10;
+                        var baseFactorInverse = baseFactorKeyLong % 2 == 0;
 
-                //        var dateFactorModifierKey = $"{date:yyyy-MM-dd}";
-                //        var dateFactorModifierKeyBytes = Encoding.ASCII.GetBytes(dateFactorModifierKey).ToList().Concat(longBytes);
-                //        var dateFactorModifierKeyLong = BitConverter.ToInt64([.. dateFactorModifierKeyBytes], 0);
-                //        var dateFactorModifierKeyString = ((decimal)baseFactorKeyLong / dateFactorModifierKeyLong).ToString();
-                //        var dateFactorModifier = short.Parse(dateFactorModifierKeyString.Substring(dateFactorModifierKeyString.Length - 2, 2));
+                        var dateFactorModifierKey = $"{date:yyyy-MM-dd}";
+                        var dateFactorModifierKeyBytes = Encoding.ASCII.GetBytes(dateFactorModifierKey).ToList().Concat(longBytes);
+                        var dateFactorModifierKeyLong = BitConverter.ToInt64([.. dateFactorModifierKeyBytes], 0);
+                        var dateFactorModifierKeyString = ((decimal)baseFactorKeyLong / dateFactorModifierKeyLong).ToString();
+                        var dateFactorModifier = short.Parse(dateFactorModifierKeyString.Substring(dateFactorModifierKeyString.Length - 2, 2));
 
-                //        var exchangeRate = Math.Round((baseFactorInverse ? 1.0 / baseFactorBase : baseFactorBase) * (0.95 + 0.001 * dateFactorModifier), 2);
+                        var exchangeRate = Math.Round((baseFactorInverse ? 1.0 / baseFactorBase : baseFactorBase) * (0.95 + 0.001 * dateFactorModifier), 2);
 
-                //        return exchangeRate;
-                //    }
-                //    catch (Exception ex)
-                //    {
-                //        throw new ServiceException(ExceptionType.CurrencyServiceCallError, ex, $"{fromCurrency} => {toCurrency}");
-                //    }
-                //}, null);
-                //task.Start();
-                //await task.WaitAsync(cancellationToken);
+                        return exchangeRate;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new ServiceException(ExceptionType.CurrencyServiceCallError, ex, $"{fromCurrency} => {toCurrency}");
+                    }
+                }, null);
+                task.Start();
+                await task.WaitAsync(cancellationToken);
 
-                return 1.14;
+                return exchangeRate;
             }
 
             return 1;
